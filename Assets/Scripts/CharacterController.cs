fe4663d900e854f3a3b6d257a8c67a0627301266
zxcv12377 <75616357+zxcv12377@users.noreply.github.com>
2022-12-01 21:10:05 +0900

    초기 프로젝트 구성

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public float MoveSpeed;
+    public float jumpPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector2(-MoveSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector2(MoveSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+}
