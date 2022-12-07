178ae08e1412cd52d6fab0f7b9f494fc2c5a821e
zxcv12377 <75616357+zxcv12377@users.noreply.github.com>
2022-12-07 14:02:10 +0900

    CharacterCotroller수정

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterController : MonoBehaviour
 {
     Rigidbody2D rigid;
     public float MoveSpeed;
     public float jumpPower;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +23,21 @@ public class CharacterController : MonoBehaviour
 
     void PlayerMove()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(new Vector2(-MoveSpeed * Time.deltaTime, 0));
-        }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(new Vector2(MoveSpeed * Time.deltaTime, 0));
         }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector2(-MoveSpeed * Time.deltaTime, 0));
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
 }
+
+
