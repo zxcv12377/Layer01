7a67e1d65078805a1c1cf133238e8229e3deef24
zxcv12377 <75616357+zxcv12377@users.noreply.github.com>
2022-12-10 13:45:58 +0900

    대시 추가

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 다음 만들것 공격 모션, 대쉬 모션, 죽는 것, 체력
-// 본격적으로 벽을 만들어야함
-// 플랫폼도 수정하고 카메라 움직임과 스카이박스 움직임도 설정해야함.
+// 1. 다음 만들것 대쉬 모션, 죽는 것, 체력
+// 2. 본격적으로 벽을 만들어야함
+// 3. 플랫폼도 수정하고 카메라 움직임과 스카이박스 움직임도 설정해야함.
+// (추가)
+// 4. 대쉬 움직임 수정하기 (해결)
 public class CharacterController : MonoBehaviour
 {
     Rigidbody2D rigid;
     Animator anim;
     SpriteRenderer spriteRenderer;
+    //TrailRenderer tr;
 
     float Hmove;
 
     //private
+    private float MoveSpeed = 5f;
+    [SerializeField] private float jumpPower = 25f;
+    private float DashPower = 10f;
+
+    private bool canDash = true;
+    private float DashTime = 0.2f;
+    private float DashCooldown = 1f;
 
     //public
-    public float MoveSpeed;
-    public float jumpPower;
+    
+    public int maxHP;
+    public int currentHP;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,57 +42,136 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (anim.GetBool("isDash"))
+        {
+            return;
+        }
+
         Hmove = Input.GetAxisRaw("Horizontal");
         PlayerMove();
+        if (Input.GetKey(KeyCode.X))
+        {
+            Attack();
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && canDash)
+        {
+            Debug.Log("Dash");
+            StartCoroutine(Dash());
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
     }
     // Update is called once per frame
     void Update()
     {
+        if (anim.GetBool("isDash"))
+        {
+            return;
+        }
         AnimationUpdate();
-    }
 
+        if(rigid.velocity.y < 0)
+        {
+            anim.SetBool("isFalling", true);
+        }
+
+        anim.SetFloat("yVelocity", rigid.velocity.y);
+    }
+    
+    // Move //
     void PlayerMove()
     {
         Vector3 movePosition = Vector3.zero;
 
-        if(Input.GetAxisRaw("Horizontal") > 0)
+        if (anim.GetBool("isDash") != true)
         {
-            movePosition = Vector3.right;
-            spriteRenderer.flipX = false;
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                movePosition = Vector3.right;
+                spriteRenderer.flipX = false;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                movePosition = Vector3.left;
+                spriteRenderer.flipX = true;
+            }
         }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            movePosition = Vector3.left;
-            spriteRenderer.flipX = true;
-        }
+        
 
         transform.position += movePosition * MoveSpeed * Time.deltaTime;
                 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (anim.GetBool("isJump") != true)
-            {
-                anim.SetBool("isJump", true);
-                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            }
-        }
+        
     }
+
+    // Collision //
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag == "Platform")
         {
             anim.SetBool("isJump", false);
+            anim.SetBool("isFalling", false);
         }
     }
 
+    // Attack //
+    public void Attack()
+    {
+        if (anim.GetBool("isAttack") != true && anim.GetBool("isDash") != true)
+        {
+            anim.SetBool("isAttack", true);
+        }
+    }
+
+    public void Jump()
+    {
+        if (anim.GetBool("isJump") != true && anim.GetBool("isDash") != true)
+        {
+            anim.SetBool("isJump", true);
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    // Dash //
+    private IEnumerator Dash()
+    {
+        float Dir = Input.GetAxisRaw("Horizontal");
+        if(Dir != 0)
+        {
+            canDash = false;
+            anim.SetBool("isDash", true);
+            float OriginalGravity = rigid.gravityScale;
+            rigid.gravityScale = 0f;
+            rigid.velocity = new Vector2(transform.localScale.x * DashPower * Dir, 0f);
+            yield return new WaitForSeconds(DashTime);
+            rigid.gravityScale = OriginalGravity;
+            anim.SetBool("isDash", false);
+            yield return new WaitForSeconds(DashCooldown);
+            canDash = true;
+        }
+    }
+
+    // Animation Control //
     void AnimationUpdate()
     {
-        if(Hmove == 0)
+        if (Hmove == 0)
         {
             anim.SetBool("isMove", false);
         }
         else anim.SetBool("isMove", true);
+    }
+
+    public void StopAttack() // Attack 애니메이션에 사용중
+    {
+        anim.SetBool("isAttack", false);
+    }
+
+    // Die //
+    void Die()
+    {
+        anim.SetTrigger("Death");
     }
 }
 
