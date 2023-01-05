9adc775430272eefef16adffbc06bbc2a29e1638
zxcv12377 <75616357+zxcv12377@users.noreply.github.com>
2023-01-05 15:00:47 +0900

    Update CharacterSelectButtonController.cs

--- Assets/Scripts/CharacterSelectButtonController.cs
+++ Assets/Scripts/CharacterSelectButtonController.cs
@@ -10,17 +10,21 @@ public class CharacterSelectButtonController : MonoBehaviour
     [SerializeField] private GameObject uitxt;
     private Text txt;
     [SerializeField] private GameObject[] SelectChracter;
-    private Button btn;
+    [SerializeField] private GameObject Warning;
     private int[] SelectCnt = {0, 0};
 
     private void Start()
     {
         txt = uitxt.GetComponent<Text>();
-        //btn = GetComponent<Button>(); 맵을 사용할까 말까 고민중
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown("enter") || Input.GetMouseButtonDown(0))
+        {
+            Warning.SetActive(false);
+        }
+
         if(EventSystem.current.currentSelectedGameObject.name == "Hero_Knight" && SelectCnt[0] == 0)
         {
             txt.text = "전사";
@@ -28,6 +32,7 @@ public class CharacterSelectButtonController : MonoBehaviour
             SelectCnt[1] = 0;
             print(EventSystem.current.currentSelectedGameObject.name);
         }
+
         else if (EventSystem.current.currentSelectedGameObject.name == "New1" && SelectCnt[1] == 0)
         {
             txt.text = "미확인";
@@ -37,14 +42,19 @@ public class CharacterSelectButtonController : MonoBehaviour
         }
     }
 
-    public void Select()
-    {
-        
-    }
-
     public void Gamestart()
     {
+        for(int i = 0; i<SelectCnt.Length; i++)
+        {
+            if(SelectCnt[i] == 1)
+            {
+                if (i == 1)
+                {
+                    Warning.SetActive(true);
+                }
+            }
 
+        }
     }
 
     public void back()
